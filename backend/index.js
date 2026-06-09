ea97906f98b6eab47f8ff4990303cd534a55c5ca
Rafael Sousa de Morais Junior <contato@rafaelmoraisjr.com>
2026-06-09 19:37:58 -0300

    feat: initial commit

---
+++ backend/index.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const morgan = require('morgan')
+const app = express();
+
+morgan.token('body', (req) => {
+  if (req.method === 'POST') {
+    return JSON.stringify(req.body)
+  }
+  return ''
+})
+
+app.use(express.json());
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+let data = [
+  {
+    "id": "1",
+    "name": "Arto Hellas",
+    "number": "040-123456"
+  },
+  {
+    "id": "2",
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523"
+  },
+  {
+    "id": "3",
+    "name": "Dan Abramov",
+    "number": "12-43-234345"
+  },
+  {
+    "id": "4",
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122"
+  }
+]
+
+app.get('/api/persons', (req, res) => {
+  res.json(data);
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = req.params.id
+  const person = data.find(person => person.id === id)
+
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).json({
+      error: 'person not found'
+    })
+  }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = req.params.id
+  data = data.filter(person => person.id !== id)
+
+  res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+  const existingPerson = data.find(person => person.name === body.name)
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: 'name or number missing'
+    })
+  } else if (existingPerson) {
+    return res.status(400).json({
+      error: 'name must be unique'
+    })
+  } else {
+    const id = Math.floor(Math.random() * 10000)
+    const person = {
+      id: String(id),
+      name: body.name,
+      number: body.number
+    }
+
+    data = data.concat(person)
+    res.status(201).json(person)
+  }
+})
+
+app.get('/info', (req, res) => {
+  const dataLength = data.length
+  const date = new Date()
+  res.send(`
+    <p>Phonebook has info for ${dataLength} people</p>
+    <p>${date}</p>
+  `)
+})
+
+const PORT = 3001;
+app.listen(PORT)
+console.log(`Server is running on http://localhost:${PORT}`);
